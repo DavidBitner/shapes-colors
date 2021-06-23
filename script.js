e4978022a0cbf66e10ac8b30edf517890061b012
David <david-bitner@hotmail.com>
2021-06-23 12:23:33 -0300

    Update script.js
    Reset, background change

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 // Variables
 const shape = document.querySelector("#shape");
 const color = document.querySelector("#color");
+const back = document.querySelector("#back");
 const bigger = document.querySelector("#bigger");
 const smaller = document.querySelector("#smaller");
+const reset = document.querySelector("#reset");
 const coord = document.querySelector("#coord");
 
 const shapes = [
@@ -48,18 +50,26 @@ function random_color() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
+// Reset UI
+function reset_ui() {
+  cur_color = "rgb(0, 0, 0)";
+  cur_shape = shapes[13];
+
+  shape.innerHTML = `Q - Shape`;
+  color.innerHTML = `W - Color`;
+  back.innerHTML = "E - Background";
+  bigger.innerHTML = "R - Bigger";
+  smaller.innerHTML = "T - Smaller";
+
+  document.body.style.backgroundColor = "#000533";
+}
+
 // Keyboard Events
 document.addEventListener("keydown", function (event) {
   if (event.code == "KeyQ") {
     // Change shape
     let cur = shapes.indexOf(cur_shape);
-
-    if (cur < 13) {
-      cur += 1;
-    } else {
-      cur = 0;
-    }
-
+    cur < 13 ? (cur += 1) : (cur = 0);
     cur_shape = shapes[cur];
     shape.innerHTML = `Q - ${cur_shape}`;
   } else if (event.code == "KeyW") {
@@ -67,9 +77,17 @@ document.addEventListener("keydown", function (event) {
     cur_color = random_color();
     color.innerHTML = `W - ${cur_color}`;
   } else if (event.code == "KeyE") {
-    console.log("Bigger");
+    console.log("Background");
+    let b_color = random_color();
+    back.innerHTML = `E - ${b_color}`;
+    document.body.style.backgroundColor = b_color;
   } else if (event.code == "KeyR") {
+    console.log("Bigger");
+  } else if (event.code == "KeyT") {
     console.log(`Smaller`);
+  } else if (event.code == "Escape") {
+    console.log(`Reset`);
+    reset_ui();
   } else {
     console.log(event);
   }
